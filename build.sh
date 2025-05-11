d81b02b3039c018876b502966d4c247ebe1565c8
Nadia <nadia.balagurova2016@yandex.ru>
2025-05-12 01:52:28 +0300

    1.4

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # Install dependencies with --no-cache-dir to reduce size
 pip3 install --no-cache-dir -r requirements.txt
 
+python3 manage.py makemigrations
+python3 manage.py migrate
 # Collect static files and apply migrations
 python3 manage.py collectstatic --noinput
-python3 manage.py migrate
